bcfbc617ac6e5d20eaffad8c115dc603bab38726
Iurii Ashaiev <iurii.ashaiev@globallogic.com>
2017-03-04 00:18:21 +0200

    Added RecyclerView.

--- app/src/main/java/com/ashaevy/syncsound/MainActivity.java
+++ app/src/main/java/com/ashaevy/syncsound/MainActivity.java
@@ -4,9 +4,16 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.google.android.exoplayer2.DefaultLoadControl;
@@ -30,8 +37,17 @@ import java.util.concurrent.TimeUnit;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
-    private SimpleExoPlayer[] mMediaPlayers = new SimpleExoPlayer[3];
+    public static final String FILES_FOLDER_UNDER_DOWNLOADS = "/music/";
+
+    private int mPlayersCount;
+    private SongPlayer[] mMediaPlayers;
     private Handler mMainHandler;
+
+    private long mGlobalClock;
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     private Runnable updateTimeLine = new Runnable() {
         @Override
@@ -39,7 +55,9 @@ public class MainActivity extends AppCompatActivity {
             if (MainActivity.this.isFinishing()) {
                 return;
             }
-            long millis = mMediaPlayers[0].getCurrentPosition();
+
+            updateGlobalClock();
+            long millis = mGlobalClock;
             String hms = String.format(Locale.getDefault(), "%02d:%02d:%02d",
                     TimeUnit.MILLISECONDS.toHours(millis),
                     TimeUnit.MILLISECONDS.toMinutes(millis) -
@@ -50,65 +68,78 @@ public class MainActivity extends AppCompatActivity {
             mMainHandler.postDelayed(updateTimeLine, 1000L);
         }
     };
+    public static final DefaultBandwidthMeter BANDWIDTH_METER = new DefaultBandwidthMeter();
+
+    private void updateGlobalClock() {
+        int playingCount = 0;
+        for (int i = 0; i < mMediaPlayers.length; i++) {
+            long currentPosition = mMediaPlayers[i].mPlayer.getCurrentPosition();
+            if (mMediaPlayers[i].mPlayer.getPlayWhenReady() && currentPosition > 0) {
+                mGlobalClock = currentPosition;
+                playingCount++;
+            }
+        }
+        ((TextView) findViewById(R.id.active_count)).
+                setText(getString(R.string.playing_text, playingCount));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
         mMainHandler = new Handler();
 
-        mMediaPlayers[0] = createExoPlayer("music/moonlight_dance.mp3");
-        mMediaPlayers[1] = createExoPlayer("music/hurricane.mp3");
-        mMediaPlayers[2] = createExoPlayer("music/beep.mp3");
+        // TODO check permissions
+        String downloadsPath = Environment.getExternalStoragePublicDirectory(Environment.
+                DIRECTORY_DOWNLOADS) + FILES_FOLDER_UNDER_DOWNLOADS;
+
+        File downloads = new File(downloadsPath);
+        File[] files = downloads.listFiles();
+        mPlayersCount = files.length;
+
+        mMediaPlayers = new SongPlayer[mPlayersCount];
+
+        // TODO do on background
+        for (int i = 0; i < mPlayersCount; i++) {
+            try {
+                mMediaPlayers[i] = new SongPlayer(files[i].getName(),
+                        createExoPlayer(files[i].getCanonicalPath()));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // specify an adapter (see also next example)
+        mAdapter = new MyAdapter(mMediaPlayers);
+        mRecyclerView.setAdapter(mAdapter);
 
         findViewById(R.id.play_all).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                start(mMediaPlayers[0]);
-                start(mMediaPlayers[1]);
-                start(mMediaPlayers[2]);
+                mGlobalClock = 0;
+                for (int i = 0; i < mPlayersCount; i++) {
+                    start(i);
+                }
             }
         });
 
-        findViewById(R.id.play1).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.stop_all).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                play(mMediaPlayers[0]);
-            }
-        });
-
-        findViewById(R.id.play2).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                play(mMediaPlayers[1]);
-            }
-        });
-
-        findViewById(R.id.play3).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                play(mMediaPlayers[2]);
-            }
-        });
-
-        findViewById(R.id.pause1).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                pause(mMediaPlayers[0]);
-            }
-        });
-
-        findViewById(R.id.pause2).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                pause(mMediaPlayers[1]);
-            }
-        });
-
-        findViewById(R.id.pause3).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                pause(mMediaPlayers[2]);
+                mGlobalClock = 0;
+                for (int i = 0; i < mPlayersCount; i++) {
+                    stop(i);
+                }
             }
         });
 
@@ -119,16 +150,15 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        for (SimpleExoPlayer player: mMediaPlayers) {
-            player.release();
+        for (SongPlayer player: mMediaPlayers) {
+            player.mPlayer.release();
         }
     }
 
     private SimpleExoPlayer createExoPlayer(String fileName) {
         // 1. Create a default TrackSelector
-        DefaultBandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
         TrackSelection.Factory videoTrackSelectionFactory =
-                new AdaptiveVideoTrackSelection.Factory(bandwidthMeter);
+                new AdaptiveVideoTrackSelection.Factory(BANDWIDTH_METER);
         TrackSelector trackSelector =
                 new DefaultTrackSelector(videoTrackSelectionFactory);
 
@@ -138,28 +168,31 @@ public class MainActivity extends AppCompatActivity {
         // 3. Create the player
         SimpleExoPlayer player =
                 ExoPlayerFactory.newSimpleInstance(this, trackSelector, loadControl);
-
-        // 4. Create a FileDataSourceFactory
-        FileDataSourceFactory fileDataSourceFactory = new FileDataSourceFactory(bandwidthMeter);
-
-        // 5. Create a ExtractorMediaSource
-        String completePath = Environment.getExternalStoragePublicDirectory(Environment.
-                DIRECTORY_DOWNLOADS) + "/" + fileName;
-        ExtractorMediaSource extractorMediaSource = new ExtractorMediaSource(Uri.fromFile(new
-                File(completePath)), fileDataSourceFactory, new DefaultExtractorsFactory(),
-                mMainHandler, new ExtractorMediaSource.EventListener() {
-            @Override
-            public void onLoadError(IOException error) {
-                Log.e(TAG, "Loading error.", error);
-            }
-        });
+        ExtractorMediaSource extractorMediaSource = createMediaSource(fileName);
 
         player.prepare(extractorMediaSource);
 
         return player;
     }
 
-    private void start(SimpleExoPlayer mediaPlayer) {
+    @NonNull
+    private ExtractorMediaSource createMediaSource(String fileName) {
+        // 4. Create a FileDataSourceFactory
+        FileDataSourceFactory fileDataSourceFactory = new FileDataSourceFactory(BANDWIDTH_METER);
+
+        return new ExtractorMediaSource(Uri.fromFile(new
+                File(fileName)), fileDataSourceFactory, new DefaultExtractorsFactory(),
+                mMainHandler, new ExtractorMediaSource.EventListener() {
+            @Override
+            public void onLoadError(IOException error) {
+                Log.e(TAG, "Loading error.", error);
+            }
+        });
+    }
+
+    private void start(int id) {
+        SimpleExoPlayer mediaPlayer = mMediaPlayers[id].mPlayer;
+
         if (!mediaPlayer.getPlayWhenReady()) {
             mediaPlayer.setPlayWhenReady(true);
         }
@@ -169,12 +202,102 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void play(SimpleExoPlayer mediaPlayer) {
-        mediaPlayer.setVolume(1.0f);
+    private void stop(int id) {
+        SimpleExoPlayer mediaPlayer = mMediaPlayers[id].mPlayer;
+
+        if (mediaPlayer.getPlayWhenReady()) {
+            mediaPlayer.setPlayWhenReady(false);
+        }
     }
 
-    private void pause(SimpleExoPlayer mediaPlayer) {
-        mediaPlayer.setVolume(0.0f);
+    public static class SongPlayer {
+        public String mSongName;
+        public SimpleExoPlayer mPlayer;
+
+        public SongPlayer(String mSongName, SimpleExoPlayer mPlayer) {
+            this.mSongName = mSongName;
+            this.mPlayer = mPlayer;
+        }
+    }
+
+    public static class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+        private SongPlayer[] mSongPlayers;
+
+        public static class ViewHolder extends RecyclerView.ViewHolder {
+            public TextView mSongNameView;
+            public Button mPlayButton;
+            public Button mPauseButton;
+
+            public ViewHolder(LinearLayout linearLayout) {
+                super(linearLayout);
+            }
+        }
+
+        // Provide a suitable constructor (depends on the kind of dataset)
+        public MyAdapter(SongPlayer[] songPlayers) {
+            mSongPlayers = songPlayers;
+        }
+
+        // Create new views (invoked by the layout manager)
+        @Override
+        public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                       int viewType) {
+            // create a new view
+            LinearLayout linearLayout = (LinearLayout) LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.song_layout, parent, false);
+            final ViewHolder vh = new ViewHolder(linearLayout);
+            vh.mSongNameView = ((TextView) linearLayout.findViewById(R.id.song_name));
+            vh.mPlayButton = ((Button) linearLayout.findViewById(R.id.play_button));
+            vh.mPauseButton = ((Button) linearLayout.findViewById(R.id.pause_button));
+
+            vh.mPlayButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    play(vh.getAdapterPosition());
+                }
+            });
+            vh.mPauseButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    pause(vh.getAdapterPosition());
+                }
+            });
+
+            return vh;
+        }
+
+        // Replace the contents of a view (invoked by the layout manager)
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            holder.mSongNameView.setText(mSongPlayers[position].mSongName);
+            float volume = mSongPlayers[position].mPlayer.getVolume();
+            if (volume > 0f) {
+                holder.mPlayButton.setEnabled(false);
+                holder.mPauseButton.setEnabled(true);
+            } else {
+                holder.mPlayButton.setEnabled(true);
+                holder.mPauseButton.setEnabled(false);
+            }
+        }
+
+        // Return the size of your dataset (invoked by the layout manager)
+        @Override
+        public int getItemCount() {
+            return mSongPlayers.length;
+        }
+
+        private void play(int id) {
+            SimpleExoPlayer mediaPlayer = mSongPlayers[id].mPlayer;
+            mediaPlayer.setVolume(1.0f);
+            notifyItemChanged(id);
+        }
+
+        private void pause(int id) {
+            SimpleExoPlayer mediaPlayer = mSongPlayers[id].mPlayer;
+            mediaPlayer.setVolume(.0f);
+            notifyItemChanged(id);
+        }
+
     }
 
 }
